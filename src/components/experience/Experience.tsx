96f1bf7a39b5df1594ded63b215930f49de2326f
unknown <107139727+Manoelpimentel96@users.noreply.github.com>
2026-08-26 21:49:33 -0300

    Update Experience.tsx

--- src/components/experience/Experience.tsx
+++ src/components/experience/Experience.tsx
@@ -1,16 +1,21 @@
+import { Container } from "@/components/layout/Container";
+
 import ExperienceHeader from "./ExperienceHeader";
 import ExperienceTimeline from "./ExperienceTimeline";
+
 export default function Experience() {
   return (
     <section
       id="experience"
-      className="w-full px-4 py-20 md:px-6 md:py-24"
+      className="w-full border-b border-border bg-background py-20 md:py-24"
     >
-      <div className="mx-auto flex w-full max-w-6xl flex-col gap-14">
-        <ExperienceHeader />
-        <ExperienceTimeline />
-      </div>
+      <Container>
+        <div className="flex w-full flex-col gap-14">
+          <ExperienceHeader />
+
+          <ExperienceTimeline />
+        </div>
+      </Container>
     </section>
   );
-
 }
